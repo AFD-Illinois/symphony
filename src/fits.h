578d0c113a193b5d09a6e402ad967ef7c61afe0a
Alex <aapandy2@illinois.edu>
2016-02-17 00:52:35 -0600

    added fits.h; fitting formulae work but still need to fix j_nu() and alpha_nu() integrations

---
+++ src/fits.h
@@ -0,0 +1,32 @@
+#ifndef SYMPHONY_FITS_H_
+#define SYMPHONY_FITS_H_
+
+double thermal_I(struct parameters * params);
+double thermal_Q(struct parameters * params);
+double thermal_V(struct parameters * params);
+double thermal_I_abs(struct parameters * params);
+double thermal_Q_abs(struct parameters * params);
+double thermal_V_abs(struct parameters * params);
+double power_law_I(struct parameters * params);
+double power_law_Q(struct parameters * params);
+double power_law_V(struct parameters * params);
+double power_law_I_abs(struct parameters * params);
+double power_law_Q_abs(struct parameters * params);
+double power_law_V_abs(struct parameters * params);
+double kappa_I(struct parameters * params);
+double kappa_Q(struct parameters * params);
+double kappa_V(struct parameters * params);
+double kappa_I_abs(struct parameters * params);
+double kappa_Q_abs(struct parameters * params);
+double kappa_V_abs(struct parameters * params);
+double check_for_errors(struct parameters * params);
+double j_nu_fit(double nu, 
+                double magnetic_field, 
+                double electron_density, 
+                double observer_angle, 
+                int distribution, 
+                int polarization
+                );
+
+#endif /* SYMPHONY_FITS_H_ */
+
