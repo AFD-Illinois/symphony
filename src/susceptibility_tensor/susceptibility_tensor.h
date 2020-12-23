8c26ee63d9103b0d1d745eb8eb520660895a6c12
agm5 <35504198+agm5@users.noreply.github.com>
2020-12-22 19:13:42 -0600

    Added chi_rho_Q_symphony, chi_rho_Q_integrand, and chi_rho_Q

--- src/susceptibility_tensor/susceptibility_tensor.h
+++ src/susceptibility_tensor/susceptibility_tensor.h
@@ -147,6 +147,22 @@ double chi_33_symphony(double nu,
             char **error_message
            );
 
+double chi_rho_Q_symphony(double nu,
+            double magnetic_field,
+            double electron_density,
+            double observer_angle,
+            int distribution,
+            int real_part,
+            double theta_e,
+            double power_law_p,
+            double gamma_min,
+            double gamma_max,
+            double gamma_cutoff,
+            double kappa,
+            double kappa_width,
+            char **error_message
+           );
+
 double I_1_of_2(double alpha, double delta);
 double I_1_analytic(double alpha, double delta);
 double I_2_analytic(double alpha, double delta);
@@ -167,6 +183,7 @@ double chi_22_integrand(double tau_prime, void * parameters);
 double chi_22_integrand_p1(double tau_prime, void * parameters);
 double chi_22_integrand_p2(double tau_prime, void * parameters);
 double chi_22_integrand_real(double tau_prime, void * parameters);
+double chi_rho_Q_integrand(double tau_prime, void * parameters);
 
 double alpha_V_integrand(double tau_prime, void * parameters);
 
@@ -193,4 +210,5 @@ double chi_23(struct parameters * p);
 double chi_32(struct parameters * p);
 double chi_13(struct parameters * p);
 double chi_31(struct parameters * p);
+double chi_rho_Q(struct parameters * p);
 #endif /* SUSCEPTIBILITY_H_ */
